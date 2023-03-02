2126c510c04ff7a51f602273e782f4023f6c80af
Deved Roy <devedroy.dr@gmail.com>
2023-03-02 10:16:23 +0530

    BLUETOOTH_CONNECT
    Requesting BLUETOOTH_CONNECT permission at runtime for Build.VERSION.SDK_INT >= Build.VERSION_CODES.S

--- app/src/main/java/com/example/runtimeperms/MainActivity.java
+++ app/src/main/java/com/example/runtimeperms/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.runtimeperms;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.example.runtimeperms.databinding.ActivityMainBinding;
@@ -18,7 +18,7 @@ import com.example.runtimeperms.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private int STORAGE_PERM_CODE = 1;
+    private final int BLUETOOTH_PERM_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +27,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.btnReqPerms.setOnClickListener(v -> {
-            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
             } else {
-                requestStoragePermissions();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    requestStoragePermissions();
+                }
             }
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     private void requestStoragePermissions() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            new AlertDialog.Builder(this).setTitle("Permission Needed").setMessage("Need this permission if you want to access files").setPositiveButton("Give permission", (dialog, which) -> {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERM_CODE);
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH_CONNECT)) {
+            new AlertDialog.Builder(this).setTitle("Permission Needed").setMessage("Need this permission if you want to access bluetooth").setPositiveButton("Give permission", (dialog, which) -> {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, BLUETOOTH_PERM_CODE);
             }).setNegativeButton("Don't give", (dialog, which) -> {
                 dialog.dismiss();
             }).create().show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERM_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, BLUETOOTH_PERM_CODE);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == STORAGE_PERM_CODE) {
+        if (requestCode == BLUETOOTH_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
             } else {
